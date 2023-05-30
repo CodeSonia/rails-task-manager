c149bc429869df7bc32fe412a959f85cbd72ecd5
Sonia Hussain <sonia_ahm@icloud.com>
2023-05-30 15:21:50 +0100

    Refactored routes, added before_action onto controller and created a partial form to be DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destory]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -19,18 +20,18 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
     @task.save
     redirect_to task_path(@task)
   end
 
   def destory
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
 
     # No need for DESTROY HTML
@@ -38,6 +39,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'tasks', to: 'tasks#index'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/new', to: 'tasks#new', as: 'new_task'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  get 'tasks/:id', to: 'tasks#show', as: 'task'  # We want to read just ONE
-  delete 'tasks/:id', to: 'tasks#destory'
-  patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'  # We want to read just ONE
+  # delete 'tasks/:id', to: 'tasks#destory'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  resources :tasks
 end
